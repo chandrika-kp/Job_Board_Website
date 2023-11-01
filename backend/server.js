802acd7d82ad18ec432c4c5a7aad9257ea7d5401
chandrika-kp <chandrika.450@gmail.com>
2023-11-02 00:09:16 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -39,4 +39,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
--- note.md
+++
@@ -1,7 +0,0 @@
-git add .
-
-git commit -m "message"
-
-git pull origin main
-
-git push origin main
