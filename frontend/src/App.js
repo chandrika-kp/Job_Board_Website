8e8610e08218f852ae3116b3c19a85c5556c95e6
chandrika-kp <chandrika.450@gmail.com>
2023-11-02 10:12:24 +0530

    client

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Global/Home';
+import SignUp from './components/Global/SignUp';
+import Login from './components/Global/Login';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signUp" element={<SignUp/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
