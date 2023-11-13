60b7342cfdd9f4f1aeaaf16cb9ea7d3adb835e7e
chandrika-kp <chandrika.450@gmail.com>
2023-11-13 22:48:21 +0530

    update

---
+++ frontend/client.md
@@ -0,0 +1,16 @@
+1)npx create-react-app .
+
+2)npm i bootstrap@5.3.2
+
+3)For routing i our application
+   npm i react-router-dom
+   For NotFound Page , given last route with path = "/*"
+   
+4)Set up redux dependencies
+
+npm i redux react-redux
+
+5)Install Axios 
+
+npm i axios
+
---
+++ frontend/src/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const NotFound = () => {
+
+    return (
+        <>
+            <div className="container text-center mt-5">
+                <h1 className="display-4">404 - Not Found</h1>
+                <p className="lead">Sorry, the page you are looking for does not exist.</p>
+            </div>
+        </>
+    )
+}
+
+
